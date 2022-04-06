4560e9d52003172104b6bbd415d949823288c80d
Saad Ahmed Siddiqui <saad.siddiqui@evolligence.com>
2022-04-06 07:14:32 +0500

    completed app

---
+++ components/index.js
@@ -0,0 +1,17 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { CircleButton, RectButton } from "./Buttons";
+import { SubInfo } from "./SubInfo";
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+};
